c2500273916b0333d740db7129774001d1513ec5
Adam Massoud <adam.massoud@finbourne.com>
2023-12-21 16:44:14 +0000

    fix test to work with SDK with pydantic and async

--- python/luisd/main.py
+++ python/luisd/main.py
@@ -1,40 +1,37 @@
 import cProfile
 import pstats
-import unittest
-from unittest.mock import patch, MagicMock
+from unittest import mock
+from unittest.mock import MagicMock, patch, AsyncMock
 
-from luisd import ApiClient
-from luisd.api.transaction_portfolios_api import TransactionPortfoliosApi
+from lusid import ApiClient
+from lusid.api.transaction_portfolios_api import TransactionPortfoliosApi
+import asyncio
 
 
-class DeserializationTests(unittest.TestCase):
-    """
-    to generate the client
-    java -jar /Users/justinblack/programming/openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar generate -i data/lusid.json -o python/luisd -g python-experimental --package-name luisd
-    """
+async def test_deserialize():
+    with open("../../data/response.json") as f:
+        data = f.read()
 
-    def test_deserialize(self):
-        with open("../../data/response.json") as f:
-            data = f.read()
+    mock_response = MagicMock()
+    mock_response.status = 200
+    mock_response.reason = "OK"
+    mock_response.data = str.encode(data)
+    mock_response.getheader.return_value = "application/json"
+    mock_response_future = asyncio.Future()
+    mock_response_future.set_result(mock_response)
 
-        mock_response = MagicMock()
-        mock_response.status = 200
-        mock_response.reason = "OK"
-        mock_response.data = str.encode(data)
-        mock_response.getheader.return_value = "application/json"
+    with patch.object(ApiClient, "request", return_value=mock_response_future):
+        api = TransactionPortfoliosApi()
 
-        with patch.object(ApiClient, "request", return_value=mock_response):
-            api = TransactionPortfoliosApi()
+        profiler = cProfile.Profile()
+        profiler.enable()
 
-            profiler = cProfile.Profile()
-            profiler.enable()
+        await api.get_transactions(scope='a', code='a')
 
-            api.get_transactions(path_params=dict(scope='a', code='a'))
-
-            profiler.disable()
-            stats = pstats.Stats(profiler).sort_stats(pstats.SortKey.CUMULATIVE)
-            stats.print_stats()
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats(pstats.SortKey.CUMULATIVE)
+        stats.print_stats()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    asyncio.run(test_deserialize())
